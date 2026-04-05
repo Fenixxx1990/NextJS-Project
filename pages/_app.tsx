555a80e088d34dbdbc9abff26d78a691e5af895a
Fenix <fenixxx1990@yandex.ru>
2026-04-05 16:56:17 +0400

    finish

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import Router from "next/router";
 import { type JSX } from "react";
 import { Noto_Sans } from "next/font/google";
+import { YMInitializer } from "react-yandex-metrika";
+import ym from "react-yandex-metrika";
+
+Router.events.on("routeChangeComplete", (url: string) => {
+  if (typeof window !== "undefined") {
+    ym("hit", url);
+  }
+});
 
 const notoSans = Noto_Sans({
   subsets: ["latin", "cyrillic"], // Подмножества символов
@@ -11,13 +20,28 @@ const notoSans = Noto_Sans({
   display: "swap", // Стратегия отображения
 });
 
-export default function App({ Component, pageProps }: AppProps): JSX.Element {
+export default function App({
+  Component,
+  pageProps,
+  router,
+}: AppProps): JSX.Element {
   return (
     <>
       <Head>
         <title>MyTop - наш лучший топ</title>
         <link key={1} rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://mc.yandex.ru" />
+        <meta
+          property="og:url"
+          content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath}
+        />
+        <meta property="og:locale" content="ru_Ru" />
       </Head>
+      <YMInitializer
+        accounts={[]}
+        options={{ webvisor: true, defer: true }}
+        version="2"
+      />
       <div className={notoSans.variable}>
         <Component {...pageProps} />
       </div>
